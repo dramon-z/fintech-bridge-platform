0b783de95c9a6f3e69a576d0ab380046936259bd
Daniel Ramon Zapata <dramonz@hotmail.com>
2026-03-04 19:44:02 -0600

    fix: test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,8 +1,7 @@
-import requests
-import sys
+from fastapi.testclient import TestClient
+from api.main import app
 
-# La URL vendría de una variable de entorno según el entorno de despliegue
-API_URL = "http://localhost:8000" 
+client = TestClient(app)
 
 def test_transfer_flow():
     print("🚀 Iniciando pruebas de despliegue...")
@@ -14,16 +13,11 @@ def test_transfer_flow():
         "txhash": "0x123456"
     }
 
-    res = requests.post(f"{API_URL}/transfer", json=payload_ok)
-    
-    print(res.json())
-    print(res.status_code)
+    response = client.post("/transfer", json=payload_ok)
 
-    if res.status_code == 200 and res.json()["status"] == "success":
-        print("✅ Test VendorA: PASSED")
-    else:
-        print(f"❌ Test VendorA: FAILED ({res.status_code})")
-        sys.exit(1)
+
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
 
     # Caso 2: TxHash inválido (debe dar 404)
     payload_fail = {
@@ -31,15 +25,6 @@ def test_transfer_flow():
         "vendor": "vendorA",
         "txhash": "0x99999"
     }
-    res_fail = requests.post(f"{API_URL}/transfer", json=payload_fail)
+    response_fail = client.post("/transfer", json=payload_fail)
 
-    if res_fail.status_code == 404:
-        print("✅ Test Invalid TxHash: PASSED (404 found)")
-    else:
-        print(f"❌ Test Invalid TxHash: FAILED (Expected 404, got {res_fail.status_code})")
-        sys.exit(1)
-
-    print("🎉 Todas las pruebas de despliegue pasaron exitosamente.")
-
-if __name__ == "__main__":
-    test_transfer_flow()
+    assert response_fail.status_code == 404
